3da8b20853747c0d68638cd5e47d484c53426937
Debaditya <ddev2636@gmail.com>
2022-09-09 19:07:02 +0530

    team

--- src/components/team/Card.js
+++ src/components/team/Card.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Card.css'
-import {AiFillInstagram,AiFillGithub,AiFillLinkedin} from 'react-icons/ai';
+import {AiFillInstagram,AiFillGithub,AiFillLinkedin, AiFillTwitterSquare} from 'react-icons/ai';
 
 const Card = ({name,image,team,insta,linkedin,github}) => {
+  let dam;let man;
+  if(linkedin!=='') {dam=linkedin;
+    man=<AiFillLinkedin/>;  
+  }
+  else{ dam=insta;
+  man=<AiFillTwitterSquare/>;
+  }
   return (
     <div className='text-white p-4 pt-8  flex flex-col justify-center'>
         <div className=''>      
-        <img src={image} className='w-[240px]  rounded-[50%] color' />
+        <img src={image} className='w-[240px] h-[280px]  rounded-[50%] color' />
         </div>
         <div className='flex justify-center mt-[12px] text-[24px] lg:text-[24px] text-orange-500'>
 
@@ -19,7 +26,7 @@ const Card = ({name,image,team,insta,linkedin,github}) => {
         <div className='flex justify-evenly text-4xl pt-5 pb-1'>
         {/* <a href={github}><AiFillGithub/></a>
         <a href={insta}><AiFillInstagram/></a> */}
-        <a href={linkedin}><AiFillLinkedin/></a>
+        <a href={dam}>{man}</a>
         </div>
         </div>
   )
