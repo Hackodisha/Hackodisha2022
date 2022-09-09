20b6d6f253b4dc98f4d54929816b83522b75a7a0
Debaditya <ddev2636@gmail.com>
2022-09-09 16:13:27 +0530

    team-lead

--- src/components/team/Card.js
+++ src/components/team/Card.js
@@ -6,19 +6,19 @@ const Card = ({name,image,team,insta,linkedin,github}) => {
   return (
     <div className='text-white p-4 pt-8  flex flex-col justify-center'>
         <div className=''>      
-        <img src={image} className='w-[240px] rounded-[50%]' />
+        <img src={image} className='w-[240px]  rounded-[50%] color' />
         </div>
-        <div className='flex justify-center mt-[8px] text-[24px] lg:text-[28px] text-orange-700'>
+        <div className='flex justify-center mt-[12px] text-[24px] lg:text-[24px] text-orange-500'>
 
         {name}
         </div>
-        <div className='flex justify-center mt-[8px] text-[24px] lg:text-[28px] text-yellow-500'>
+        <div className='flex justify-center mt-[8px] text-[24px] lg:text-[26px] text-yellow-400'>
             
         {team}
         </div>
         <div className='flex justify-evenly text-4xl pt-5 pb-1'>
-        <a href={github}><AiFillGithub/></a>
-        <a href={insta}><AiFillInstagram/></a>
+        {/* <a href={github}><AiFillGithub/></a>
+        <a href={insta}><AiFillInstagram/></a> */}
         <a href={linkedin}><AiFillLinkedin/></a>
         </div>
         </div>
--- src/components/team/TeamData.js
+++ src/components/team/TeamData.js
@@ -1,64 +1,67 @@
+import Akash from './assets/pics/Akash.jpg'
+import Gourab from './assets/pics/Gourab.png'
+import Shantanu from './assets/pics/Shantanu.jpg'
+import Amreet from './assets/pics/Amreet.png'
+import Shreya from './assets/pics/Shreya.jpeg'
 export default [
     {
         
-      name: 'Bertie Yates',      
+      name: 'Kumar Shantanu',      
       image:
-      "https://thumbs.dreamstime.com/b/courier-rides-scooter-express-delivery-motorcycle-service-vector-illustration-180980568.jpg",
-      team:'Public Relations Team',
+      Shantanu,
+      team:'Lead Organiser',
       insta:'',
       github:'',
-      linkedin:'',
+      linkedin:'https://www.linkedin.com/in/kumar-shantanu01/',
 
     },
     {
         
-      name: 'John Smith',      
+      name: 'Gourab Nanda',      
       image:
-      "https://thumbs.dreamstime.com/b/courier-rides-scooter-express-delivery-motorcycle-service-vector-illustration-180980568.jpg",
-      team:'Technical Team',
+      Gourab,
+      team:'Lead Organiser',
       insta:'',
       github:'',
-      linkedin:'',
+      linkedin:'https://www.linkedin.com/in/gourab-nanda-0b27aa173/',
     },
+    // {
+        
+    //   name: 'Manjeet Dhayal',      
+    //   image:
+    //   "https://thumbs.dreamstime.com/b/courier-rides-scooter-express-delivery-motorcycle-service-vector-illustration-180980568.jpg",
+    //   team:'Tech Lead',
+    //   insta:'',
+    //   github:'',
+    //   linkedin:'',
+    // },
     {
         
-      name: 'Victor',      
-      image:
-      "https://thumbs.dreamstime.com/b/courier-rides-scooter-express-delivery-motorcycle-service-vector-illustration-180980568.jpg",
-      team:'Sponsorship Team',
+      name: 'Amreet Kumar Khuntia',      
+      image:      Amreet,
+      team:'DC and Streaming Lead',
       insta:'',
       github:'',
-      linkedin:'',
+      linkedin:'https://www.linkedin.com/in/amreet-khuntia-15193220b/',
+    },
+  {
+      
+      name: 'Akash Sahu',      
+      image: Akash,      
+      team:'Management Lead',
+      insta:'',
+      github:'',
+      linkedin:'https://www.linkedin.com/in/akashsahu006',
+    },
+  {
+      
+      name: 'Shreya Khetan',      
+      image:
+     Shreya,
+      team:'PR and Outreach Lead',
+      insta:'',
+      github:'',
+      linkedin:'https://www.linkedin.com/in/shreyakhetan',
     },
     
-    {
-        
-      name: 'Steve',      
-      image:
-      "https://thumbs.dreamstime.com/b/courier-rides-scooter-express-delivery-motorcycle-service-vector-illustration-180980568.jpg",
-      team:'Design Team',
-      insta:'',
-      github:'',
-      linkedin:'',
-    },
-    {
-        
-      name: 'Dolly',      
-      image:
-      "https://thumbs.dreamstime.com/b/courier-rides-scooter-express-delivery-motorcycle-service-vector-illustration-180980568.jpg",
-      team:'Show More',
-      insta:'',
-      github:'',
-      linkedin:'',
-    },
-    {
-        
-      name: 'Dummy',      
-      image:
-      "https://thumbs.dreamstime.com/b/courier-rides-scooter-express-delivery-motorcycle-service-vector-illustration-180980568.jpg",
-      team:'Show More',
-      insta:'',
-      github:'',
-      linkedin:'',
-    },
 ]
