a35aab2e86ddf0a3110bcc257c10bc33ece0afde
manjeetdhayal <manjeetdhayal786@gmail.com>
2022-09-09 18:35:19 +0530

    Show More Fixed: teams page

--- src/components/team/TeamData.js
+++ src/components/team/TeamData.js
@@ -1,67 +1,66 @@
-import Akash from './assets/pics/Akash.jpg'
-import Gourab from './assets/pics/Gourab.png'
-import Shantanu from './assets/pics/Shantanu.jpg'
-import Amreet from './assets/pics/Amreet.png'
-import Shreya from './assets/pics/Shreya.jpeg'
+import Akash from "./assets/pics/Akash.jpg";
+import Gourab from "./assets/pics/Gourab.png";
+import Shantanu from "./assets/pics/Shantanu.jpg";
+import Amreet from "./assets/pics/Amreet.png";
+import Shreya from "./assets/pics/Shreya.jpeg";
+import Manjeet from "./assets/pics/Manjeet.jpeg";
 export default [
-    {
-        
-      name: 'Kumar Shantanu',      
-      image:
-      Shantanu,
-      team:'Lead Organiser',
-      insta:'',
-      github:'',
-      linkedin:'https://www.linkedin.com/in/kumar-shantanu01/',
+  {
+    name: "Manjeet Dhayal",
+    image: Manjeet,
+    team: "Lead Organiser",
+    insta: "",
+    github: "",
+    linkedin: "",
+  },
+  {
+    name: "Kumar Shantanu",
+    image: Shantanu,
+    team: "Lead Organiser",
+    insta: "",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/kumar-shantanu01/",
+  },
+  {
+    name: "Gourab Nanda",
+    image: Gourab,
+    team: "Lead Organiser",
+    insta: "",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/gourab-nanda-0b27aa173/",
+  },
+  // {
 
-    },
-    {
-        
-      name: 'Gourab Nanda',      
-      image:
-      Gourab,
-      team:'Lead Organiser',
-      insta:'',
-      github:'',
-      linkedin:'https://www.linkedin.com/in/gourab-nanda-0b27aa173/',
-    },
-    // {
-        
-    //   name: 'Manjeet Dhayal',      
-    //   image:
-    //   "https://thumbs.dreamstime.com/b/courier-rides-scooter-express-delivery-motorcycle-service-vector-illustration-180980568.jpg",
-    //   team:'Tech Lead',
-    //   insta:'',
-    //   github:'',
-    //   linkedin:'',
-    // },
-    {
-        
-      name: 'Amreet Kumar Khuntia',      
-      image:      Amreet,
-      team:'DC and Streaming Lead',
-      insta:'',
-      github:'',
-      linkedin:'https://www.linkedin.com/in/amreet-khuntia-15193220b/',
-    },
+  //     name: 'Manjeet Dhayal',
+  //     image:
+  //     "https://thumbs.dreamstime.com/b/courier-rides-scooter-express-delivery-motorcycle-service-vector-illustration-180980568.jpg",
+  //     team:'Tech Lead',
+  //     insta:'',
+  //     github:'',
+  //     linkedin:'',
+  //   },
   {
-      
-      name: 'Akash Sahu',      
-      image: Akash,      
-      team:'Management Lead',
-      insta:'',
-      github:'',
-      linkedin:'https://www.linkedin.com/in/akashsahu006',
-    },
+    name: "Amreet Kumar Khuntia",
+    image: Amreet,
+    team: "DC and Streaming Lead",
+    insta: "",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/amreet-khuntia-15193220b/",
+  },
   {
-      
-      name: 'Shreya Khetan',      
-      image:
-     Shreya,
-      team:'PR and Outreach Lead',
-      insta:'',
-      github:'',
-      linkedin:'https://www.linkedin.com/in/shreyakhetan',
-    },
-    
-]
+    name: "Akash Sahu",
+    image: Akash,
+    team: "Management Lead",
+    insta: "",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/akashsahu006",
+  },
+  {
+    name: "Shreya Khetan",
+    image: Shreya,
+    team: "PR and Outreach Lead",
+    insta: "",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/shreyakhetan",
+  },
+];
