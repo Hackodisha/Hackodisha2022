ad895982bc043dd434e23975be64110ae4d07d1e
Debaditya <ddev2636@gmail.com>
2022-09-10 14:53:42 +0530

    photos

--- src/components/team/TeamData.js
+++ src/components/team/TeamData.js
@@ -32,8 +32,8 @@ import Manjeet from './assets/pics/Manjeet.jpeg'
 
 export default [
   {
-    name: "Gourab Nanda",
-    image: Gourab,
+  name: "Gourab Nanda",
+    image: "https://res.cloudinary.com/webwiznitr/image/upload/v1662734474/Hackodisha2.0/person.jpg_-_Gourab_Nanda_ntarfk.png",
     team: "Lead Organiser",
     insta: "",
     github: "",
@@ -41,7 +41,7 @@ export default [
   },
   {
     name: "Kumar Shantanu",
-    image: Shantanu,
+    image: "https://res.cloudinary.com/webwiznitr/image/upload/v1662734593/Hackodisha2.0/shantanu_-_kumar_shantanu_fibvsp.jpg",
     team: "Lead Organiser",
     insta: "",
     github: "",
@@ -67,101 +67,133 @@ export default [
   //     linkedin:'',
   //   },
   {
-    name: "Amreet Kumar Khuntia",
-    image: Amreet,
-    team: "DC and Streaming Lead",
+    name: "Shreya Khetan",
+    image: "https://res.cloudinary.com/webwiznitr/image/upload/v1662798043/Hackodisha2.0/E94A5797-1C79-42E7-A903-0323DF6AB1E4_-_Shreya_Khetan_qng32p.jpg",
+    team: "PR and Outreach Lead",
     insta: "",
     github: "",
-    linkedin: "https://www.linkedin.com/in/amreet-khuntia-15193220b/",
+    linkedin: "https://www.linkedin.com/in/shreyakhetan",
   },
+  
   {
     name: "Akash Sahu",
-    image: Akash,
+    image: "https://res.cloudinary.com/webwiznitr/image/upload/v1662734429/Hackodisha2.0/IMG_20220820_214228_-_Akash_xssm6j.jpg",
     team: "Management Lead",
     insta: "",
     github: "",
     linkedin: "https://www.linkedin.com/in/akashsahu006",
   },
   {
+    name: "Aporanta Mukhopadhyay",
+    image: "https://res.cloudinary.com/webwiznitr/image/upload/v1662795653/Hackodisha2.0/296266442_5094810280630876_2272629197845313131_n_jgtdwu.jpg",
+    team: "Content Lead",
+    insta: "",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/aporanta-mukhopadhyay-744892210/?originalSubdomain=in",
+  },
+  {
+    name: "Amreet Kumar Khuntia",
+    image: "https://res.cloudinary.com/webwiznitr/image/upload/v1662734118/Hackodisha2.0/Amreet_kumar_khuntia_2_-_Amreet_Ent_yo5ni7.png",
+    team: "DC and Streaming Lead",
+    insta: "",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/amreet-khuntia-15193220b/",
+  },
+  {
+    name: "Rituraj Tripathy",
+    image: "https://res.cloudinary.com/webwiznitr/image/upload/v1662800044/Hackodisha2.0/PicsArt_09-14-10.09.41_-_Rituraj_Tripathy_vzjvwt_rb0lvl.jpg",
+    team: "Design Lead",
+    insta: "",
+    github: "",
+    linkedin: "https://www.linkedin.com/in/rituraj-tripathy-a666aa20b/",
+  },
+  
+  {
+    name: 'Sai Sambhab Chaini',      
+    image:
+    "https://res.cloudinary.com/webwiznitr/image/upload/v1662734441/Hackodisha2.0/person_-_Webwiz_ervebq.jpg",
+    team:'Organiser',
+    insta:'',
+    github:'',
+    linkedin:'https://www.linkedin.com/in/sai-sambhab-chaini', 
+  },
+  {
       
-      name: 'Shreya Khetan',      
-      image:
-     Shreya,
-      team:'PR and Outreach Lead',
-      insta:'',
-      github:'',
-      linkedin:'https://www.linkedin.com/in/shreyakhetan',
-    },
-    {
-      name: "Rituraj Tripathy",
-      image: "https://res.cloudinary.com/webwiznitr/image/upload/v1631692861/Hackodisha-Team/PicsArt_09-14-10.09.41_-_Rituraj_Tripathy_vzjvwt.jpg",
-      team: "Design Lead",
-      insta: "",
-      github: "",
-      linkedin: "https://www.linkedin.com/in/rituraj-tripathy-a666aa20b/",
-    },
-    {
-      name: "Aporanta Mukhopadhyay",
-      image: "https://res.cloudinary.com/webwiznitr/image/upload/v1662787929/Hackodisha2.0/296266442_5094810280630876_2272629197845313131_n_jgtdwu.jpg",
-      team: "Content Lead",
-      insta: "",
-      github: "",
-      linkedin: "https://www.linkedin.com/in/aporanta-mukhopadhyay-744892210/?originalSubdomain=in",
-    },
-    {
-      name: 'Sai Sambhab Chaini',      
-      image:
-      sai,
-      team:'Organiser',
-      insta:'',
-      github:'',
-      linkedin:'https://www.linkedin.com/in/sai-sambhab-chaini', 
-    },
+    name: 'Jyoti Prakash Sethy ',      
+    image:
+    "https://res.cloudinary.com/webwiznitr/image/upload/v1662734420/Hackodisha2.0/Jyoti_Prakash_-_Jyoti_Prakash_Sethy_z5gftx.png",
+    team:'Organiser',
+    insta:'',
+    github:'',
+    linkedin:'https://www.linkedin.com/in/jyotiprakash25/',
+  },
+  {
+      
+    name: 'Aashirbad Sabat ',      
+    image:
+    "https://res.cloudinary.com/webwiznitr/image/upload/v1662797970/Hackodisha2.0/IMG-20220220-WA0056_1_-_AASHIRBAD_SABAT_kfjhpy.jpg",
+    team:'Organiser',
+    insta:'',
+    github:'',
+    linkedin:'https://www.linkedin.com/in/aashirbad-sabat-443974228',
+  },
+  {
+      
+    name: 'Anubhab patra ',      
+    image:
+   "https://res.cloudinary.com/webwiznitr/image/upload/v1662797513/Hackodisha2.0/SAHIL_CROPPED_PIC_-_Anubhab_Patra_w3pbrk.jpg",
+    team:'Organiser',
+    insta:'',
+    github:'',
+    linkedin:'https://www.linkedin.com/in/almanac123/',
+  },
+  {
+        
+    name: 'Debaditya Arnab',      
+    image:
+   "https://res.cloudinary.com/webwiznitr/image/upload/v1662797557/Hackodisha2.0/IMG_20220828_103154_-_Debaditya_Arnab_qfe6sb.jpg",
+    team:'Organiser',
+    insta:'',
+    github:'',
+    linkedin:'https://www.linkedin.com/in/debaditya-arnab-701978226/',
+  },
+  {
+    name: 'Rohan Kumar Muduli',      
+    image:
+   "https://res.cloudinary.com/webwiznitr/image/upload/v1662798001/Hackodisha2.0/IMG-20210322-WA0000_-_Rohan_Muduli_jrbort_377e63.jpg",
+    team:'Organiser',
+    insta:'',
+    github:'',
+    linkedin:'https://www.linkedin.com/in/rohan-kumar-muduli-6853a7245/',
+  }
+  ,
   {
       
       name: 'Khair Alanam',      
       image:
-     Khair,
-      team:'PR and Outreach Team',
+     "https://res.cloudinary.com/webwiznitr/image/upload/v1662734299/Hackodisha2.0/IMG_20220826_153035_-_Khair_z9osjq.jpg",
+      team:'Organiser',
       insta:'',
       github:'',
       linkedin:'https://www.linkedin.com/in/khair-alanam-b27b69221/',
     },
+  
   {
       
-      name: 'Rohan Kumar Muduli ',      
+      name: 'Anushray Jain',      
       image:
-     Rohan,
-      team:'PR and Outreach Team',
-      insta:'',
-      github:'',
-      linkedin:'https://www.linkedin.com/in/rohan-kumar-muduli-6853a7245/',
-    },
-  {
-      
-      name: 'Anushray ',      
-      image:
-    anushrey,
+    "https://res.cloudinary.com/webwiznitr/image/upload/v1662734265/Hackodisha2.0/IMG_20220814_223653_522_-_Anushrey_Jain_lt1kv9.jpg",
       team:'PR and Outreach Team',
       insta:'',
       github:'',
       linkedin:'https://linkedin.com/in/anushrxy',
     },
-  {
-      
-      name: 'Anubhab patra ',      
-      image:
-     anubhab,
-      team:'PR and Outreach Team',
-      insta:'',
-      github:'',
-      linkedin:'https://www.linkedin.com/in/almanac123/',
-    },
+  
   {
       
       name: 'Sumant Tirkey',      
       image:
-     sumant,
+     "https://res.cloudinary.com/webwiznitr/image/upload/v1662798127/Hackodisha2.0/20220227_161402_-_Sumant._XD_uqd1bz.jpg",
       team:'PR and Outreach Team',
       insta:'',
       github:'',
@@ -171,7 +203,7 @@ export default [
       
       name: 'Vivek jha',      
       image:
-     vivek,
+     "https://res.cloudinary.com/webwiznitr/image/upload/v1662797438/Hackodisha2.0/WhatsApp_Image_2022-08-28_at_6.41.06_PM_-_Vivek_Jha_xw6fdm.jpg",
       team:'Tech Team',
       insta:'',
       github:'',
@@ -181,82 +213,98 @@ export default [
       
       name: 'Shirsho Das Roy ',      
       image:
-      Shirsho,
+      "https://res.cloudinary.com/webwiznitr/image/upload/v1662797589/Hackodisha2.0/IMG_20220828_180018_-_Shirsho_Das_Roy_a5dxir.jpg",
       team:'Tech Team',
       insta:'',
       github:'',
       linkedin:'https://www.linkedin.com/in/shirsho-das-roy-303151202',
     },
-    {
+    
+      {
         
-        name: 'Debaditya Arnab',      
+        name: 'Lakshmi Haritha ',      
         image:
-       Debaditya,
-        team:'Tech Team',
-        insta:'',
-        github:'',
-        linkedin:'https://www.linkedin.com/in/debaditya-arnab-701978226/',
-      },
-  {
-      
-      name: 'Anshu Teja Chirra',      
-      image:
-     img1,
-      team:'Design Team',
-      insta:'',
-      github:'',
-      linkedin:'https://www.linkedin.com/in/anshu-teja-chirra-72a687215',
-    },
-    {
-      
-      name: 'Sai Praneetha  ',      
-      image:
-      img2,
-      team:'Design Team',
-      insta:'',
-      github:'',
-      linkedin:'https://www.linkedin.com/in/saipraneetha',
-    },
-    {
-        
-        name: 'Annadanam Lakshmi Haritha ',      
-        image:
-       img3,
+       "https://res.cloudinary.com/webwiznitr/image/upload/v1662797820/Hackodisha2.0/IMG_20220831_122819_2_gjj5rl.jpg",
         team:'Design Team',
         insta:'',
         github:'',
         linkedin:'https://www.linkedin.com/in/lakshmi-haritha-annadanam-4b8632201',
       },
-    {
+      {
       
-      name: 'Adya Verma ',      
-      image:
-      img4,
-      team:'Content Team',
-      insta:'',
-      github:'',
-      linkedin:'https://www.linkedin.com/in/adya-verma-b5241824a/',
-    },
+        name: 'Anshu Teja Chirra',      
+        image:
+       "https://res.cloudinary.com/webwiznitr/image/upload/v1662797537/Hackodisha2.0/IMG_20220826_225648_-_Anshu_Teja_Chirra_tn5azk.jpg",
+        team:'Design Team',
+        insta:'',
+        github:'',
+        linkedin:'https://www.linkedin.com/in/anshu-teja-chirra-72a687215',
+      },
+      {
+      
+        name: 'Sai Praneetha  ',      
+        image:
+        "https://res.cloudinary.com/webwiznitr/image/upload/v1662797852/Hackodisha2.0/IMG_20220519_183349_1_-_Sai_Praneetha_kusrkd.jpg",
+        team:'Design Team',
+        insta:'',
+        github:'',
+        linkedin:'https://www.linkedin.com/in/saipraneetha',
+      },
+  
+    
+    
+    
     {
         
         name: 'Sonali Sahu',      
         image:
-       img5,
+       "https://res.cloudinary.com/webwiznitr/image/upload/v1662798155/Hackodisha2.0/1263619291_-_Sonali_ii01f7.jpg",
         team:'Content Team',
         insta:'',
         github:'',
         linkedin:'https://www.linkedin.com/in/sonali-sahu-7510a3211/',
       },
     {
+        
+        name: 'Manali Mishra',      
+        image:
+       "https://res.cloudinary.com/webwiznitr/image/upload/v1662800503/Hackodisha2.0/WhatsApp_Image_2022-09-10_at_2.20.58_PM_kvxl5c.jpg",
+        team:'Content Team',
+        insta:'',
+        github:'',
+        linkedin:'https://www.linkedin.com/in/manali-mishra-6015511a5/?originalSubdomain=in',
+      },
+    {
+        
+        name: 'Huruthi Muggala',      
+        image:
+       "https://res.cloudinary.com/webwiznitr/image/upload/v1662800460/Hackodisha2.0/WhatsApp_Image_2022-09-10_at_2.20.03_PM_koipn1.jpg",
+        team:'Content Team',
+        insta:'',
+        github:'',
+        linkedin:'https://www.linkedin.com/in/hruthi-muggalla/?originalSubdomain=in',
+      },
+    {
+        
+        name: 'Priyansha Nayak',      
+        image:
+       "https://res.cloudinary.com/webwiznitr/image/upload/v1662800401/Hackodisha2.0/WhatsApp_Image_2022-09-10_at_2.27.32_PM_c8xryh.jpg",
+        team:'Content Team',
+        insta:'',
+        github:'',
+        linkedin:'https://www.linkedin.com/in/priyansha-nayak-98b18a205/?originalSubdomain=in',
+      },
+      {
       
-      name: 'Aashirbad Sabat ',      
-      image:
-      aashirbad,
-      team:'Sponsorship Team',
-      insta:'',
-      github:'',
-      linkedin:'https://www.linkedin.com/in/aashirbad-sabat-443974228',
-    },
+        name: 'Adya Verma ',      
+        image:
+        "https://res.cloudinary.com/webwiznitr/image/upload/v1662798074/Hackodisha2.0/B612_20220825_001806_619_-_Adya_Verma_fhpvua.jpg",
+        team:'Content Team',
+        insta:'',
+        github:'',
+        linkedin:'https://www.linkedin.com/in/adya-verma-b5241824a/',
+      },
+    
     // {
         
     //     name: 'Abinash Gupta',      
@@ -271,7 +319,7 @@ export default [
         
         name: 'Sibasis Malla',      
         image:
-       sibasis,
+       "https://res.cloudinary.com/webwiznitr/image/upload/v1662797920/Hackodisha2.0/IMG-20220820-WA0084_-_Sibasis_Malla_njabtb.jpg",
         team:'Management Team',
         insta:'',
         github:'',
@@ -281,28 +329,58 @@ export default [
       
       name: 'Lalit Kumar Sahu  ',      
       image:
-      lalit,
+      "https://res.cloudinary.com/webwiznitr/image/upload/v1662797496/Hackodisha2.0/Remini20211211174830895_-_Lalit_Sahu_huw4wa.jpg",
       team:'Management Team',
       insta:'',
       github:'',
       linkedin:'https://www.linkedin.com/in/lalit-kumar-sahu-7bba61219',
     },
     {
+      
+      name: 'Siba Prasad Das  ',      
+      image:
+      "https://res.cloudinary.com/webwiznitr/image/upload/v1662798096/Hackodisha2.0/20220814_232029_-_Niharika_Das_Das_uqdijy.jpg",
+      team:'Management Team',
+      insta:'',
+      github:'',
+      linkedin:'https://www.linkedin.com/in/siba-prasad-das-b61b5a220',
+    },
+    {
         
         name: 'Simun Sarangi ',      
         image:
-       simun,
+       "https://res.cloudinary.com/webwiznitr/image/upload/v1662797944/Hackodisha2.0/IMG-20220807-WA0126_-_simun_sarangi_bsrsrl.jpg",
         team:'Management Team',
         insta:'',
         github:'',
         linkedin:'https://www.linkedin.com/in/simun-sarangi-661038232',
+      },
+      {
+        
+        name: 'Anubhuti Pandey',      
+        image:
+        "https://res.cloudinary.com/webwiznitr/image/upload/v1662797419/Hackodisha2.0/WhatsApp_Image_2022-08-28_at_5.37.06_PM_-_Anubhuti_pandey_wfuyqk.jpg",
+        team:'Management Team',
+        insta:'',
+        github:'',
+        linkedin:'https://www.linkedin.com/in/anubhuti-pandey-34b5a0225/?originalSubdomain=in',
+      },
+    {
+        
+        name: 'Neeraj Rathod',      
+        image:
+       "https://res.cloudinary.com/webwiznitr/image/upload/v1662801395/Hackodisha2.0/neeeraj_prds98.jpg",
+        team:'Management Team',
+        insta:'',
+        github:'',
+        linkedin:'https://www.linkedin.com/in/neeraj-rathod-0b54281b9?originalSubdomain=in',
       },
     
     {
         
         name: 'Piyush Paikroy',      
         image:
-      piyush,
+      "https://res.cloudinary.com/webwiznitr/image/upload/v1662734633/Hackodisha2.0/WhatsApp_Image_2022-07-03_at_9.46.35_PM_-_PIYUSH_PAIKROY_hoserr.jpg",
         team:'Management Team',
         insta:'',
         github:'',
@@ -318,46 +396,19 @@ export default [
     //     github:'',
     //     linkedin:'https://www.linkedin.com/in/komal-deegwal-35b31a206',
     //   },
-    {
-      
-      name: 'Jyoti Prakash Sethy ',      
-      image:
-      jyoti,
-      team:'Management Team',
-      insta:'',
-      github:'',
-      linkedin:'https://www.linkedin.com/in/jyotiprakash25/',
-    },
+    
     {
         
         name: 'Sanjeet Patro',      
         image:
-       sanjeet,
+       "https://res.cloudinary.com/webwiznitr/image/upload/v1662797893/Hackodisha2.0/IMG20220621135906_-_Sanjeet_Patro_vsxhjb.jpg",
         team:'Management Team',
         insta:'',
         github:'',
         linkedin:'https://www.linkedin.com/in/sanjeet-patro-6a1067202',
       },
-    {
-        
-        name: 'Anubhuti Pandey',      
-        image:
-       anubhuti,
-        team:'Management Team',
-        insta:'',
-        github:'',
-        linkedin:'https://www.linkedin.com/in/anubhuti-pandey-34b5a0225/?originalSubdomain=in',
-      },
-    {
-      
-      name: 'Siba Prasad Das  ',      
-      image:
-      siba,
-      team:'Management Team',
-      insta:'',
-      github:'',
-      linkedin:'https://www.linkedin.com/in/siba-prasad-das-b61b5a220',
-    },
+    
+    
     
     
 ]
