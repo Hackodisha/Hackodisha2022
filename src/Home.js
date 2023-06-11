60bca760d9f86f2f25e8703a1b034ad4c2122997
Ankit Kiran <95562947+Aankirz@users.noreply.github.com>
2023-06-12 01:43:37 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,9 @@ import PrizePage from "./components/prizes2/PrizePage";
 
 const Home = () => {
   return (
+    <div className="h-screen w-screen">
    <iframe src="https://embed.lottiefiles.com/animation/79561"></iframe>
+    <div>
 //     <Routes>
 //       <Route exact path="/prizes" element={<PrizePage />} />
 //       <Route exact path="/team" element={<Team />} />
