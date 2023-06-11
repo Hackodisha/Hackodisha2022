89ad094fe63d3950c9eb54c7cc8db38d5f3f21de
Ankit Kiran <95562947+Aankirz@users.noreply.github.com>
2023-06-12 01:50:57 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ import PrizePage from "./components/prizes2/PrizePage";
 const Home = () => {
   return (
     <div className="h-screen w-screen bg-black text-white flex items-center justify-center">
-     <h1>3.0 <br>COMING SOON</h1>
+     <h1>3.0 <br/> COMING SOON </h1>
     </div>
 //     <Routes>
 //       <Route exact path="/prizes" element={<PrizePage />} />
